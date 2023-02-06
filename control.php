0b3a0ec613948fbe69876dc4fbe91cb94aef5056
Crixus21 <124603356+Crixus21@users.noreply.github.com>
2023-02-06 19:01:22 +0100

    Add files via upload

--- control.php
+++ control.php
@@ -59,6 +59,35 @@ include_once 'const.php';
     $fajlok = array_values($fajlok);
     
     
+    //Hétváltás
+    
+    $sql = "select infData from informacio where infid = 2";
+    $result = mysqli_query($dbc, $sql);
+    list($lastWeekChange) = mysqli_fetch_row($result);
+    $lastWeekChange = new DateTime($lastWeekChange);
+    $diff = (new DateTime())->getTimestamp() - $lastWeekChange->getTimestamp();
+    
+    
+    if(($diff / 60 / 60 / 24) > 7)
+    {
+        
+        $dbc->begin_transaction();
+        
+        $sql = "select uid, weeklyProgress from userek where weeklyProgress = (select max(weeklyProgress) from userek)";
+        $result = mysqli_query($dbc, $sql);
+        list($maxUid, $maxProgress) = mysqli_fetch_row($result);
+        
+        $newProgress = (string)$maxUid . "/" . (string)$maxProgress;
+        $dbc->query("update informacio set infData = '$newProgress' where infid = 1");
+        $dbc->query("update userek set weeklyProgress = 0");
+        $refreshDate = new DateTime();
+        $refreshDate = $refreshDate->format("Y-m-d H:i:s");
+        $dbc->query("update informacio set infData = '$refreshDate' where infid = 2");
+        
+        $dbc->commit();
+    }
+    
+    
     //Eseménykezelés
     
     if($feladat === 'regisztracio')
